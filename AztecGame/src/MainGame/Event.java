e4d302a437dd65e914f68b65c63829f90c94dbc8
tws2xa <tws2xa@virginia.edu>
2014-10-08 15:23:11 -0400

    Get and Set all over the place.

--- AztecGame/src/MainGame/Event.java
+++ AztecGame/src/MainGame/Event.java
@@ -19,4 +19,60 @@ public class Event {
 	
 	
 	
+	
+	//ALL the getters and setters!
+	public String getEventID() {
+		return eventID;
+	}
+	public void setEventID(String eventID) {
+		this.eventID = eventID;
+	}
+	public String getEventType() {
+		return eventType;
+	}
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+	public ArrayList<String> getPossibleLocations() {
+		return possibleLocations;
+	}
+	public void setPossibleLocations(ArrayList<String> possibleLocations) {
+		this.possibleLocations = possibleLocations;
+	}
+	public ArrayList<String> getReqParty() {
+		return reqParty;
+	}
+	public void setReqParty(ArrayList<String> reqParty) {
+		this.reqParty = reqParty;
+	}
+	public String getIntroText() {
+		return introText;
+	}
+	public void setIntroText(String introText) {
+		this.introText = introText;
+	}
+	public ArrayList<ResponseOption> getResponseOptions() {
+		return responseOptions;
+	}
+	public void setResponseOptions(ArrayList<ResponseOption> responseOptions) {
+		this.responseOptions = responseOptions;
+	}
+	public HashMap<String, String> getAdvice() {
+		return advice;
+	}
+	public void setAdvice(HashMap<String, String> advice) {
+		this.advice = advice;
+	}
+	public String getFleePassText() {
+		return fleePassText;
+	}
+	public void setFleePassText(String fleePassText) {
+		this.fleePassText = fleePassText;
+	}
+	public String getFleeFailText() {
+		return fleeFailText;
+	}
+	public void setFleeFailText(String fleeFailText) {
+		this.fleeFailText = fleeFailText;
+	}
 }
--- AztecGame/src/MainGame/ResponseOption.java
+++ AztecGame/src/MainGame/ResponseOption.java
@@ -26,4 +26,80 @@ public class ResponseOption {
 	private ArrayList<Integer> loseResourceChange;
 	private ArrayList<Integer> losePartyStatChange;
 	
+	
+	
+	
+	//Get and Set ALL the things!
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public ArrayList<Integer> getResourceStatCost() {
+		return resourceStatCost;
+	}
+	public void setResourceStatCost(ArrayList<Integer> resourceStatCost) {
+		this.resourceStatCost = resourceStatCost;
+	}
+	public ArrayList<Integer> getPartyStatRequirement() {
+		return partyStatRequirement;
+	}
+	public void setPartyStatRequirement(ArrayList<Integer> partyStatRequirement) {
+		this.partyStatRequirement = partyStatRequirement;
+	}
+	public ArrayList<String> getResourceModifiers() {
+		return resourceModifiers;
+	}
+	public void setResourceModifiers(ArrayList<String> resourceModifiers) {
+		this.resourceModifiers = resourceModifiers;
+	}
+	public ArrayList<String> getPartyStatModifiers() {
+		return partyStatModifiers;
+	}
+	public void setPartyStatModifiers(ArrayList<String> partyStatModifiers) {
+		this.partyStatModifiers = partyStatModifiers;
+	}
+	public String getPassText() {
+		return passText;
+	}
+	public void setPassText(String passText) {
+		this.passText = passText;
+	}
+	public String getWinText() {
+		return winText;
+	}
+	public void setWinText(String winText) {
+		this.winText = winText;
+	}
+	public ArrayList<Integer> getWinResourceChange() {
+		return winResourceChange;
+	}
+	public void setWinResourceChange(ArrayList<Integer> winResourceChange) {
+		this.winResourceChange = winResourceChange;
+	}
+	public ArrayList<Integer> getWinPartyStatChange() {
+		return winPartyStatChange;
+	}
+	public void setWinPartyStatChange(ArrayList<Integer> winPartyStatChange) {
+		this.winPartyStatChange = winPartyStatChange;
+	}
+	public String getLoseText() {
+		return loseText;
+	}
+	public void setLoseText(String loseText) {
+		this.loseText = loseText;
+	}
+	public ArrayList<Integer> getLoseResourceChange() {
+		return loseResourceChange;
+	}
+	public void setLoseResourceChange(ArrayList<Integer> loseResourceChange) {
+		this.loseResourceChange = loseResourceChange;
+	}
+	public ArrayList<Integer> getLosePartyStatChange() {
+		return losePartyStatChange;
+	}
+	public void setLosePartyStatChange(ArrayList<Integer> losePartyStatChange) {
+		this.losePartyStatChange = losePartyStatChange;
+	}
 }
