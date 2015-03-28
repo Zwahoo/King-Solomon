0a5bc153b33e81ec4acee109b6b191fed574031d
NickGrab <NickGrabar@gmail.com>
2015-03-28 16:40:04 -0400

    Apparently, I have to do this?

--- AztecGame/src/MainGame/MainGame.java
+++ AztecGame/src/MainGame/MainGame.java
@@ -61,7 +61,7 @@ public class MainGame {
 	public static final int MIN_PARTY_SIZE = 2;
 
 
-	public static final boolean DISCO_MODE = true;
+	public static final boolean DISCO_MODE = false;
 	
 //github.com/Zwahoo/King-Solomon.git
 	//-----------------------------------------
